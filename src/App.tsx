1f5c6239f08571b9d7af1fdbf9b74f43ea5d3ca9
shinjuho2 <sinjuho2@gmail.com>
2023-12-21 18:22:19 +0900

    파일 분리

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import { createGlobalStyle, styled } from 'styled-components';
 import { RouterProvider, createBrowserRouter } from "react-router-dom"
 import Clock from './routes/clock';
 import Timer from './routes/timer';
+import NavBar from './components/navbar';
+import * as H from './styled-components/styledClock';
 
 const router = createBrowserRouter([
   {
@@ -15,44 +17,13 @@ const router = createBrowserRouter([
   }
 ])
 
-const Wrapper = styled.div`
-  height: 100vh;
-  display: flex;
-  justify-content: center;
-`;
-
-const Button = styled.label`
-  padding: 10px 0px;
-  color: #1d9bf0;
-  text-align: center;
-  border-radius: 20px;
-  border: 1px solid #1d9bf0;
-  font-size: 14px;
-  font-weight: 600;
-  cursor: pointer;
-`
-
-const GlobalStyles = createGlobalStyle`
-  * {
-    box-sizing: border-box;
-  }
-  body {
-    background-color: #000000;
-    color:white;
-    font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
-  }
-`;
-
 function App() {
 
-  const [darkMode, setDarkMode] = useState(false);
-
   return (
-    <Wrapper>
-      <Button onClick={() => {setDarkMode(true)}}>DarkMode🔄️</Button>
-      { darkMode ? <GlobalStyles /> : null }
+    <H.Wrapper>
+      <NavBar/>
       <RouterProvider router={router} />
-    </Wrapper>
+    </H.Wrapper>
   );
 }
 
---
+++ src/styled-components/styledClock.tsx
@@ -0,0 +1,16 @@
+import { createGlobalStyle, styled } from "styled-components";
+
+export const Wrapper = styled.div`
+  text-align: center;
+`;
+
+export const GlobalStyles = createGlobalStyle`
+  * {
+    box-sizing: border-box;
+  }
+  body {
+    background-color: #000000;
+    color:white;
+    font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  }
+`;
