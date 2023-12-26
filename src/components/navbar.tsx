d79b6145874032eb5aca12b774f0ce548d350c06
shinjuho2 <sinjuho2@gmail.com>
2023-12-26 18:39:36 +0900

    navBar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,9 +1,38 @@
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
-import Button from 'react-bootstrap/Button';
-import { useState } from 'react';
+import React, { useState } from 'react';
+import styled from 'styled-components';
+
+const NavBarContainer = styled.div`
+  background-color: #333;
+  padding: 10px;
+  color: white;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const Logo = styled.div`
+  font-size: 1.5rem;
+  font-weight: bold;
+`;
+
+const NavItems = styled.div`
+  display: flex;
+  gap: 20px;
+
+  a {
+    color: white;
+    text-decoration: none;
+    font-weight: bold;
+
+    &:hover {
+      text-decoration: underline;
+    }
+  }
+`;
+
+const Button = styled.button`
+  
+`
 
 function NavBar() {
 
@@ -12,34 +41,17 @@ function NavBar() {
         setDarkMode(!darkMode)
     };
 
-    console.log(darkMode)
-
-    return (
-        <Navbar expand="lg" className="bg-body-tertiary">
-            <Container>
-                <Navbar.Brand href="/">React-Bootstrap</Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="me-auto">
-                        <Nav.Link href="/">clock</Nav.Link>
-                        <Nav.Link href="/timer">timer</Nav.Link>
-                        <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.2">
-                                Another action
-                            </NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                            <NavDropdown.Divider />
-                            <NavDropdown.Item href="#action/3.4">
-                                Separated link
-                            </NavDropdown.Item>
-                        </NavDropdown>
-                        <Button onClick={darkModeHandler}>DarkMode🔄️</Button>
-                    </Nav>
-                </Navbar.Collapse>
-            </Container>
-        </Navbar>
-    );
-}
+  return (
+    <NavBarContainer>
+      <Logo>My Website</Logo>
+      <NavItems>
+        <a href="/">Home</a>
+        <a href="/about">About</a>
+        <a href="/contact">Contact</a>
+        <Button onClick={darkModeHandler}>DarkMode🔄️</Button>
+      </NavItems>
+    </NavBarContainer>
+  );
+};
 
 export default NavBar;
