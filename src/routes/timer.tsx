f2801f0757042da499ae249e4bc465f0df3e53e0
shinjuho2 <sinjuho2@gmail.com>
2024-01-15 20:14:12 +0900

    timer

--- src/routes/timer.tsx
+++ src/routes/timer.tsx
@@ -1,13 +1,23 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import * as H from '../styled-components/styledClock';
 import { TimerContainer } from './../styled-components/styledClock';
 
 export default function Timer() {
     const [sec, setSec] = useState(0);
-    
+
+    useEffect(() => {
+      const intervalId = setInterval(() => {
+        setSec((prevSec) => prevSec + 1);
+      }, 1000);
+  
+      return () => clearInterval(intervalId);
+    }, []);
+  
+    const Timer = new Date(sec * 1000).toISOString().substring(11, 8);
+
     return(
         <H.TimerContainer>
-          <H.TimerText></H.TimerText>
+          <H.TimerText>{Timer}</H.TimerText>
         </H.TimerContainer>
     )
 }
