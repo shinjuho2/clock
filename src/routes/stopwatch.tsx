7d2b754867dcb36e55b44f85ffbd9cededb4bd2a
shinjuho2 <sinjuho2@gmail.com>
2024-01-19 23:44:10 +0900

    stopwatch

--- src/routes/stopwatch.tsx
+++ src/routes/stopwatch.tsx
@@ -2,24 +2,40 @@ import { useEffect, useState } from "react"
 import * as H from '../styled-components/styledClock';
 
 export default function Timer() {
-    const [sec, setSec] = useState(0);
+    const [time, setTime] = useState(0);
     const [isRunning, setIsRunning] = useState(false);
     const [lapTimes, setLapTimes] = useState<number[]>([]);
 
     useEffect(() => {
-      const intervalId = setInterval(() => {
-        setSec((prevSec) => prevSec + 1);
-      }, 1000);
+      let intervalId: NodeJS.Timeout;
+  
+      if (isRunning) {
+        intervalId = setInterval(() => {
+          setTime((prevTime) => prevTime + 1);
+        }, 1000);
+      }
   
       return () => clearInterval(intervalId);
-    }, []);
+    }, [isRunning]);
   
-    const Timer = new Date(sec * 1000).toISOString().substring(11, 8);
-    
-
+    const handleToggle = () => {
+      setIsRunning((prevState) => !prevState);
+    };
+  
+    const handleLap = () => {
+      setLapTimes((prevLapTimes) => [...prevLapTimes, time]);
+    };
+  
+    const handleReset = () => {
+      setTime(0);
+      setLapTimes([]);
+    };
+  
+    const formattedTime = new Date(time * 1000).toISOString().substr(11, 8);
+  
     return(
         <H.TimerContainer>
-          <H.TimerText>{`Timer: ${sec} seconds`}</H.TimerText>
+          <H.TimerText></H.TimerText>
         </H.TimerContainer>
     )
 }
