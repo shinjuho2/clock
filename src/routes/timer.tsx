bfd35f30769bd683bfbfbd4a0a63cb94c8c8c620
shinjuho2 <sinjuho2@gmail.com>
2024-01-30 23:39:50 +0900

    timer set

--- src/routes/timer.tsx
+++ src/routes/timer.tsx
@@ -1,3 +1,10 @@
+import { useState } from "react";
+
 export default function Timer() {
+    const [hours, setHours] = useState<number>(0);
+    const [minutes, setMinutes] = useState<number>(0);
+    const [seconds, setSeconds] = useState<number>(0);
+    const [timerOn, setTimerOn] = useState<boolean>(false);
+    
     return <h1>timer</h1>    
 }
