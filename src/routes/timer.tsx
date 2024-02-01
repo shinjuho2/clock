763eada713dfcb21ec035182e85d77c8bb7735aa
shinjuho2 <sinjuho2@gmail.com>
2024-02-01 20:01:52 +0900

    timer

--- src/routes/timer.tsx
+++ src/routes/timer.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function Timer() {
     const [hours, setHours] = useState<number>(0);
@@ -6,9 +6,48 @@ export default function Timer() {
     const [seconds, setSeconds] = useState<number>(0);
     const [timerOn, setTimerOn] = useState<boolean>(false);
 
+    useEffect(() => {
+        let intervalId: NodeJS.Timeout;
+    
+        if (timerOn) {
+          intervalId = setInterval(() => {
+            if (seconds === 0 && minutes === 0 && hours === 0) {
+              clearInterval(intervalId);
+              setTimerOn(false);
+              alert('타이머가 종료되었습니다.');
+            } else if (seconds === 0 && minutes === 0) {
+              setHours((prevHours) => prevHours - 1);
+              setMinutes(59);
+              setSeconds(59);
+            } else if (seconds === 0) {
+              setMinutes((prevMinutes) => prevMinutes - 1);
+              setSeconds(59);
+            } else {
+              setSeconds((prevSeconds) => prevSeconds - 1);
+            }
+          }, 1000);
+        }
+    
+        return () => clearInterval(intervalId);
+      }, [timerOn, seconds, minutes, hours]);
+    
+      const handleStart = () => {
+        setTimerOn(true);
+      };
+    
+      const handleStop = () => {
+        setTimerOn(false);
+      };
+    
+      const handleReset = () => {
+        setTimerOn(false);
+        setHours(0);
+        setMinutes(0);
+        setSeconds(0);
+      };
+
     return (
         <div>
-            
         </div>
     );
 }
