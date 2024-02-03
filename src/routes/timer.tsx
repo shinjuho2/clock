95a799a674a0fe701cfac39d07dbd8e4051874a9
shinjuho2 <sinjuho2@gmail.com>
2024-02-03 21:22:18 +0900

    timer

--- src/routes/timer.tsx
+++ src/routes/timer.tsx
@@ -1,72 +1,94 @@
-import { useEffect, useState } from "react";
+import React, { useState, useEffect } from 'react';
+import styled from 'styled-components';
 
-export default function Timer() {
-    const [hours, setHours] = useState<number>(0);
-    const [minutes, setMinutes] = useState<number>(0);
-    const [seconds, setSeconds] = useState<number>(0);
-    const [timerOn, setTimerOn] = useState<boolean>(false);
+const TimerContainer = styled.div`
+  text-align: center;
+`;
 
-    useEffect(() => {
-        let intervalId: NodeJS.Timeout;
+const TimerInput = styled.input`
+  margin: 0 5px;
+  width: 50px;
+`;
 
-        if (timerOn) {
-            intervalId = setInterval(() => {
-                if (seconds === 0 && minutes === 0 && hours === 0) {
-                    clearInterval(intervalId);
-                    setTimerOn(false);
-                    alert('타이머가 종료되었습니다.');
-                } else if (seconds === 0 && minutes === 0) {
-                    setHours((prevHours) => prevHours - 1);
-                    setMinutes(59);
-                    setSeconds(59);
-                } else if (seconds === 0) {
-                    setMinutes((prevMinutes) => prevMinutes - 1);
-                    setSeconds(59);
-                } else {
-                    setSeconds((prevSeconds) => prevSeconds - 1);
-                }
-            }, 1000);
+const TimerButton = styled.button`
+  margin: 0 5px;
+  padding: 5px 10px;
+  background-color: #4caf50;
+  color: white;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+`;
+
+const Timer: React.FC = () => {
+  const [hours, setHours] = useState<number>(0);
+  const [minutes, setMinutes] = useState<number>(0);
+  const [seconds, setSeconds] = useState<number>(0);
+  const [timerOn, setTimerOn] = useState<boolean>(false);
+
+  useEffect(() => {
+    let intervalId: NodeJS.Timeout;
+
+    if (timerOn) {
+      intervalId = setInterval(() => {
+        if (seconds === 0 && minutes === 0 && hours === 0) {
+          clearInterval(intervalId);
+          setTimerOn(false);
+          alert('타이머가 종료되었습니다.');
+        } else if (seconds === 0 && minutes === 0) {
+          setHours((prevHours) => prevHours - 1);
+          setMinutes(59);
+          setSeconds(59);
+        } else if (seconds === 0) {
+          setMinutes((prevMinutes) => prevMinutes - 1);
+          setSeconds(59);
+        } else {
+          setSeconds((prevSeconds) => prevSeconds - 1);
         }
+      }, 1000);
+    }
 
-        return () => clearInterval(intervalId);
-    }, [timerOn, seconds, minutes, hours]);
+    return () => clearInterval(intervalId);
+  }, [timerOn, seconds, minutes, hours]);
 
-    const handleStart = () => {
-        setTimerOn(true);
-    };
+  const handleStart = () => {
+    setTimerOn(true);
+  };
 
-    const handleStop = () => {
-        setTimerOn(false);
-    };
+  const handleStop = () => {
+    setTimerOn(false);
+  };
 
-    const handleReset = () => {
-        setTimerOn(false);
-        setHours(0);
-        setMinutes(0);
-        setSeconds(0);
-    };
+  const handleReset = () => {
+    setTimerOn(false);
+    setHours(0);
+    setMinutes(0);
+    setSeconds(0);
+  };
 
-    return (
-        <div>
-            <div>
-                <label>
-                    Hours:
-                    <input type="number" value={hours} onChange={(e) => setHours(parseInt(e.target.value))} />
-                </label>
-                <label>
-                    Minutes:
-                    <input type="number" value={minutes} onChange={(e) => setMinutes(parseInt(e.target.value))} />
-                </label>
-                <label>
-                    Seconds:
-                    <input type="number" value={seconds} onChange={(e) => setSeconds(parseInt(e.target.value))} />
-                </label>
-            </div>
-            <div>
-                <button onClick={handleStart}>Start</button>
-                <button onClick={handleStop}>Stop</button>
-                <button onClick={handleReset}>Reset</button>
-            </div>
-        </div>
-    );
-}
+  return (
+    <TimerContainer>
+      <div>
+        <label>
+          Hours:
+          <TimerInput type="number" value={hours} onChange={(e) => setHours(parseInt(e.target.value))} />
+        </label>
+        <label>
+          Minutes:
+          <TimerInput type="number" value={minutes} onChange={(e) => setMinutes(parseInt(e.target.value))} />
+        </label>
+        <label>
+          Seconds:
+          <TimerInput type="number" value={seconds} onChange={(e) => setSeconds(parseInt(e.target.value))} />
+        </label>
+      </div>
+      <div>
+        <TimerButton onClick={handleStart}>Start</TimerButton>
+        <TimerButton onClick={handleStop}>Stop</TimerButton>
+        <TimerButton onClick={handleReset}>Reset</TimerButton>
+      </div>
+    </TimerContainer>
+  );
+};
+
+export default Timer;
