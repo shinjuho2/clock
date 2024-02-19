fc9888375abcdbcbe46596556b2d36bcdc1b6b4b
shinjuho2 <sinjuho2@gmail.com>
2024-02-19 23:20:48 +0900

    .

--- src/routes/stopwatch.tsx
+++ src/routes/stopwatch.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react"
 import * as H from '../styled-components/styledClock';
 
 export default function Stopwatch() {
+  
     const [time, setTime] = useState(0);
     const [isRunning, setIsRunning] = useState(false);
     const [lapTimes, setLapTimes] = useState<number[]>([]);
